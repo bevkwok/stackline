c6a5dcc739def99a79c88a62a6a5fe4c2dd3622e
Beverly Kwok <yan.kwok.b@gmail.com>
2024-01-06 18:19:10 -0800

    Add redux to product info page

--- src/Components/ProductInfo.tsx
+++ src/Components/ProductInfo.tsx
@@ -4,22 +4,26 @@ import {
   Label,
   Segment,
 } from 'semantic-ui-react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
 
-const ProductInfo: FC  = () => (
-  <Segment className='product product-info'>
-    <div className="product-details">
-      <img src={ProductImage} className="product-image" alt="logo" />
-      <p className="product-title">Shark Ninja</p>
-      <p className="product-description">Magic Bullet NutriBullet 12-Piece</p>
-      <p className="product-description">High-Speed Blender/Mixer System</p>
-    </div>
-    <div className="product-tags">
-    <Label basic>Pantry</Label>
-    <Label basic>Obsolete</Label>
-    <Label basic>Blender</Label>
-    <Label basic>Lightning Deal</Label>
-    </div>
-  </Segment>
-);
+const ProductInfo: FC  = () => {
+  const { products } = useSelector((state: RootState) => state.product);
+  const currentProduct = products?.[0];
+
+  return (
+    <Segment className='product product-info'>
+      <div className="product-details">
+        <img src={ProductImage} className="product-image" alt="logo" />
+        <p className="product-title">{currentProduct?.title}</p>
+        <p className="product-description">Magic Bullet NutriBullet 12-Piece</p>
+        <p className="product-description">High-Speed Blender/Mixer System</p>
+      </div>
+      <div className="product-tags">
+        {currentProduct?.tags?.map((tag: string)=> <Label basic>{tag}</Label>)}
+      </div>
+    </Segment>
+  );
+};
   
 export default ProductInfo;
