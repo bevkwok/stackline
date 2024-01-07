e25c73d108caecdda495f28d55d6151972e305e3
Beverly Kwok <yan.kwok.b@gmail.com>
2024-01-06 20:10:15 -0800

    Create product sales chart

--- src/Components/ProductSalesChart.tsx
+++ src/Components/ProductSalesChart.tsx
@@ -1,52 +1,77 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
+import { LineChart, Line, ResponsiveContainer, XAxis, YAxis, Tooltip } from "recharts";
+import { RootState } from "../store";
+import { useSelector } from "react-redux";
 const ProductSalesChart: FC  = () => {
-  const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
+  interface SalesData {
+    weekEnding: string;
+    retailSales: number;
+    wholesaleSales: number;
+    unitsSold: number;
+    retailerMargin: number;
+    month?: string;
+  };
+
+  const { products } = useSelector((state: RootState) => state.product);
+  const months: string[] = [
+    "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+    "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
   ];
 
-  return (<></>
-  );
+  let prevMonth = "";
+  const salesData: SalesData[] | undefined = products?.[0]?.sales?.map((sale: SalesData) => {
+    const monthIndex: number = parseInt(sale?.weekEnding?.split("-")?.[1], 10) - 1;
+    let curMonth = months[monthIndex];
+    if (curMonth === prevMonth) {
+      curMonth = "";
+    }
+    else {
+      prevMonth = curMonth;
+    }
+
+    return {
+      ...sale,
+      month: curMonth
+    };
+  });
+
+  return (<ResponsiveContainer width="99%" height="95%">
+    <LineChart
+      data={salesData}
+      margin={{
+        top: 20,
+        right: 20,
+        left: 20,
+        bottom: 20,
+      }}
+    >
+      <XAxis 
+        dataKey="month" 
+        interval={0} 
+        tickLine={false} 
+        padding={{ left: 10, right: 10 }} 
+        ticks={months} 
+        stroke="#cccccc"
+        tick={{ fill: "#b0b0b0" }}
+      />
+      <YAxis hide={true} padding={{ top: 100, bottom: 100 }} />
+      <Tooltip />
+      <Line 
+        type="monotone" 
+        dataKey="wholesaleSales" 
+        stroke="#46A8F6" 
+        dot={false}
+        strokeWidth={2} 
+      />
+      <Line 
+        type="monotone" 
+        dataKey="retailSales" 
+        stroke="#A8B0C7" 
+        dot={false}
+        strokeWidth={2} 
+      />
+    </LineChart>
+  </ResponsiveContainer>);
 };
   
 export default ProductSalesChart;
